59cfbfe84dab277b24862d1af0e37730c0664eb0
HiroZar <hirozarhz@gmail.com>
2023-12-31 17:33:10 -0500

    v0.001

---
+++ Public/Js/table.js
@@ -0,0 +1,31 @@
+function buscarProducto() {
+    const productId = $('#productId').val();
+
+    // Realizar la solicitud AJAX para cargar el contenido del archivo CSV
+    $.ajax({
+        type: "GET",
+        url: '/Public/db/productos.csv',  // Reemplaza 'productos.csv' con el nombre real de tu archivo CSV
+        dataType: "text",
+        success: function (data) {
+            const rows = data.split('\n');
+            const productTableBody = $('#productTable tbody');
+            productTableBody.empty();  // Limpiar la tabla antes de agregar nuevas filas
+
+            // Procesar cada fila del CSV
+            rows.forEach(row => {
+                const columns = row.split(',');
+                const codigo = columns[0];
+                const descripcion = columns[1];
+                const peso = columns[2];
+                // Agregar a la tabla solo si el ID coincide
+                if (codigo === productId) {
+                    const newRow = `<tr><td>${codigo}</td><td>${descripcion}</td><td>${peso}</td></tr>`;
+                    productTableBody.append(newRow);
+                }
+            });
+        },
+        error: function (error) {
+            console.error("Error al cargar el archivo CSV:", error);
+        }
+    });
+}
