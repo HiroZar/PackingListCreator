a9ec9b611345810cb2f585c540ea4b0f5a4c0406
HiroZar <hirozarhz@gmail.com>
2024-01-01 09:10:09 -0500

    v0.008

--- Public/Js/table.js
+++ Public/Js/table.js
@@ -1,6 +1,8 @@
 function buscarProducto() {
     const productId = $('#productId').val();
     const productCant = $('#productCantidad').val();
+    const productDetalle = $('#productDetalle').val();
+    const codigoExistente = verificarCodigoExistente(productId);
     let item = 0;
     var radios = document.getElementsByName('list-radio');
     var valorSeleccionado = '';
@@ -11,44 +13,69 @@ function buscarProducto() {
             break; // Salir del bucle una vez que se encuentre el valor seleccionado
         }
     }
-
-    // Realizar la solicitud AJAX para cargar el contenido del archivo CSV  
-    $.ajax({
-        type: "GET",
-        url: 'Public/db/productos.csv',  // Reemplaza 'productos.csv' con el nombre real de tu archivo CSV
-        dataType: "text",
-        success: function (data) {
-            const rows = data.split('\n');
-            const productTableBody = $('#productTable tbody');
-            // productTableBody.empty();  // Limpiar la tabla antes de agregar nuevas filas
-
-            // Procesar cada fila del CSV
+    if (productId.trim() !== ''){
+        if (productDetalle.trim() !== '') {
+            if (productCant.trim() !== ''){
+                if (!codigoExistente) {
+                                   // Realizar la solicitud AJAX para cargar el contenido del archivo CSV  
+                $.ajax({
+                    type: "GET",
+                    url: 'Public/db/productos.csv',  // Reemplaza 'productos.csv' con el nombre real de tu archivo CSV
+                    dataType: "text",
+                    success: function (data) {
+                        const rows = data.split('\n');
+                        const productTableBody = $('#productTable tbody');
+                        // productTableBody.empty();  // Limpiar la tabla antes de agregar nuevas filas
             
-            rows.forEach(row => {
-                
-                const columns = row.split(',');
-                const codigo = columns[0];
-                const descripcion = columns[1];
-                const peso = columns[2];
-                const um = columns[3];
-
-
-                // Agregar a la tabla solo si el ID coincide
-                const pesobruto = (peso*productCant).toFixed(2);
-
-
-                if (codigo === productId) {
-                    item ++;
-                    const newRow = `<tr class="${valorSeleccionado}"><td class="border-2 border-black">${productTableBody.find('tr').length + 1}</td><td class="border-2 border-black cantidad">${productCant}</td><td class="border-2 border-black">${codigo}</td><td class="border-2 border-black">${descripcion}</td><td class="border-2 border-black">${um}</td><td class="border-2 border-black">${peso}</td><td class="border-2 border-black pesobruto">${pesobruto}</td></tr>`;
-                    productTableBody.append(newRow);
-                    sumacantidad();
-                    sumaPesoBruto() 
+                        // Procesar cada fila del CSV
+                        
+                        rows.forEach(row => {
+                            
+                            const columns = row.split(',');
+                            const codigo = columns[0];
+                            const descripcion = columns[1];
+                            const peso = columns[2];
+                            const um = columns[3];
+            
+            
+                            // Agregar a la tabla solo si el ID coincide
+                            const pesobruto = (peso*productCant).toFixed(2);
+            
+            
+                            if (codigo === productId) {
+                                item ++;
+                                const newRow = `<tr class="${valorSeleccionado}"><td class="border-2 border-black">${productTableBody.find('tr').length + 1}</td><td class="border-2 border-black cantidad">${productCant}</td><td class="border-2 border-black">${codigo}</td><td class="border-2 border-black">${descripcion}</td><td class="border-2 border-black">${um}</td><td class="border-2 border-black">${peso}</td><td class="border-2 border-black pesobruto">${pesobruto}</td></tr>`;
+                                productTableBody.append(newRow);
+                                sumacantidad();
+                                sumaPesoBruto();
+                                $('#productId').val('');
+                                $('#productCantidad').val('');
+                                $('#productDetalle').val('');
+                                $('#productId').focus();
+                            }
+                        });
+                    },
+                    error: function (error) {
+                        console.error("Error al cargar el archivo CSV:", error);
+                    }
+                }); 
+                } else {
+                    mostrarAdvertencia('El producto ya está en la tabla');
                 }
-            });
-        },
-        error: function (error) {
-            console.error("Error al cargar el archivo CSV:", error);
+
+            }
+            else{
+                
+                mostrarAdvertencia('Debe agregar la cantidad');
+            }
+        }   
+        else {
+            mostrarError('Producto no encontrado')
         }
-    });
+    }
+    else{
+        mostrarAdvertencia('Debe agregar el código del producto');
+    }
+    
     
 }
--- Public/Js/validaciones.js
+++ Public/Js/validaciones.js
@@ -30,6 +30,38 @@ $('#productId').on('input', function() {
     });
 
 });
+$('#productContenedor').on('input', function(){
+    const productId = $('#productContenedor').val();
+    const contenedor = document.getElementById('nombreContenedor');
+    contenedor.textContent = productId.toString();
+});
+$('#productPrecinto').on('input', function(){
+    const productPrecinto = $('#productPrecinto').val();
+    const precinto = document.getElementById('nombrePrecinto');
+    precinto.textContent = productPrecinto.toString();
+});
+$('#productFactura').on('input', function(){
+    const productFactura = $('#productFactura').val();
+    const Factura = document.getElementById('nombreFactura');
+    Factura.textContent = productFactura.toString();
+});
+
+function mostrarExtra() {
+    const datosExtras = document.getElementById('DatosExtras');
+    datosExtras.classList.remove('hidden', 'h-0');
+    datosExtras.classList.add('h-auto');
+  }
+  
+  function ocultarExtra() {
+    const datosExtras = document.getElementById('DatosExtras');
+    datosExtras.classList.remove('h-auto');
+    datosExtras.classList.add('h-0');
+    setTimeout(() => {
+      datosExtras.classList.add('hidden');
+    }, 500); // Ajusta el tiempo de espera según la duración de la transición
+  }
+  
+
 function sumacantidad() {
     const celdasCantidad = document.querySelectorAll('#productTable .cantidad');
     let sumaCantidades = 0;
@@ -38,15 +70,54 @@ function sumacantidad() {
         sumaCantidades += cantidad;
       }); 
     const elementoSuma = document.getElementById('cuentabultos');
-    elementoSuma.textContent = sumaCantidades.toString().padStart(4, '0');
+    elementoSuma.textContent = sumaCantidades.toString();
+}
+
+function verificarCodigoExistente(productId) {
+    // Obtener todas las filas de la tabla
+    const filas = document.querySelectorAll('#productTable tbody tr');
+
+    // Verificar si el código ya existe en alguna fila
+    for (let i = 0; i < filas.length; i++) {
+        const codigoEnFila = filas[i].cells[2].textContent.trim(); // Índice 2 para la tercera columna (base 0)
+
+        if (codigoEnFila === productId) {
+            return true; // El código ya existe
+        }
+    }
+
+    return false; // El código no existe
 }
 function sumaPesoBruto() {
     const celdasPesos = document.querySelectorAll('#productTable .pesobruto');
     let sumaPesos = 0;
+
     celdasPesos.forEach((celda) => {
-        const Pesos = parseInt(celda.textContent, 10) || 0; // Convertir el texto a número, o usar 0 si no se puede convertir
-        sumaPesos += Pesos;
-      }); 
+        const peso = parseFloat(celda.textContent) || 0; // Convertir el texto a número de punto flotante, o usar 0 si no se puede convertir
+        sumaPesos += peso;
+    });
+
+    // Formatear la suma con dos decimales
+    const sumaPesosFormateada = sumaPesos.toFixed(2);
+
+    // Actualizar el elemento HTML con la suma formateada
     const elementoPesos = document.getElementById('cuentapesos');
-    elementoPesos.textContent = sumaPesos.toString().padStart(4, '0');
+    elementoPesos.textContent = sumaPesosFormateada;
+}
+
+function mostrarAdvertencia(a) {
+    Swal.fire({
+        icon: 'warning',
+        title: '¡Advertencia!',
+        text: a,
+            confirmButtonColor: '#1056e3',
+    });
+}
+
+function mostrarError(a) {
+    Swal.fire({
+        icon: 'error',
+        title: '¡Error!',
+        text: a,
+    });
 }
