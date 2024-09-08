d68b45aac58c94a5503f8549c2e063386e1dc7dd
HiroZar <86934006+HiroZar@users.noreply.github.com>
2024-09-07 19:11:01 -0500

    Update pdf.js

--- Public/Js/pdf.js
+++ Public/Js/pdf.js
@@ -1,9 +1,6 @@
 
 function exportarAPDF() {
-    // Seleccionar el elemento que quieres convertir a PDF
     const elemento = document.querySelector('.exportar');
- 
-    // Opciones de configuración para la conversión
     const opciones = {
         margin: 10,
         filename: 'PackingList',
@@ -12,7 +9,6 @@ function exportarAPDF() {
         jsPDF: { unit: 'mm', format: 'a4', orientation: 'landscape' }
     };
  
-    // Usar html2pdf para generar el PDF
     irArriba();
     html2pdf(elemento, opciones)
         .then(() => {
@@ -23,26 +19,3 @@ function exportarAPDF() {
         });
  }
  
-function exportarBPDF() {
-    // Seleccionar el elemento que quieres convertir a PDF
-    const elemento = document.querySelector('.exportar');
- 
-    // Opciones de configuración para la conversión
-    const opciones = {
-        margin: 10,
-        filename: 'PackingList',
-        image: { type: 'jpeg', quality: 1 },
-        html2canvas: { scale: 3 },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-    };
- 
-    // Usar html2pdf para generar el PDF
-    irArriba();
-    html2pdf(elemento, opciones)
-        .then(() => {
-            mostrarSuccess('Boleta de pago generada exitosamente.');
-        })
-        .catch((error) => {
-            mostrarError('Error al general la boleta.')
-        });
- }
