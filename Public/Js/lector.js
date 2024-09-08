c23e29e15521d0d3e99acadfbba9b78cbe9b0bf3
HiroZar <86934006+HiroZar@users.noreply.github.com>
2024-09-07 19:11:20 -0500

    Update lector.js

--- Public/Js/lector.js
+++ Public/Js/lector.js
@@ -1,21 +1,14 @@
 $(document).ready(function () {
-    // Ruta del archivo CSV
     const filePath = '/Public/db/productos.csv';
-
-    // Realizar la solicitud AJAX para cargar el contenido del archivo CSV
     $.ajax({
         type: "GET",
         url: filePath,
         dataType: "text",
         success: function (data) {
             const rows = data.split('\n');
-
-            // Procesar cada fila del CSV
             rows.forEach(row => {
                 const columns = row.split(',');
                 console.log(columns);
-                // Puedes hacer algo con cada columna, por ejemplo:
-                // columns.forEach(column => console.log(column));
             });
         },
         error: function (error) {
